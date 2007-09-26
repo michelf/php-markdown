12b770648e49eb39a86ac93fad34ea9ddf7aa21c
Michel Fortin <michel.fortin@michelf.com>
2007-09-26 14:37:49 -0400

    More version number changes.

--- markdown.php
+++ markdown.php
@@ -62,7 +62,7 @@ function Markdown($text) {
 Plugin Name: Markdown
 Plugin URI: http://www.michelf.com/projects/php-markdown/
 Description: <a href="http://daringfireball.net/projects/markdown/syntax">Markdown syntax</a> allows you to write using an easy-to-read, easy-to-write plain text format. Based on the original Perl version by <a href="http://daringfireball.net/">John Gruber</a>. <a href="http://www.michelf.com/projects/php-markdown/">More...</a>
-Version: 1.0.1j
+Version: 1.0.1k
 Author: Michel Fortin
 Author URI: http://www.michelf.com/
 */
