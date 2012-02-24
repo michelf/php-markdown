7ba56c7b2869c6979ecaf3e916ccc6ed3fdeebc7
Saurabh Kumar <saurabh@saurabhworld.in>
2012-02-25 01:22:45 +0530

    add comments

--- web-app/markdown2html.php
+++ web-app/markdown2html.php
@@ -10,14 +10,19 @@ else {
 	$return['error'] = false;
 	
 	include ('../markdown.php');
+	
+	//get input
 	$text = $_POST['markdown_text'];
 
+	//process the input
 	$html = markdown($text);
 
+	//store the result to be returned
 	$return['msg'] = $html;
 }
 
 
+//return the result back.
 //echo json_encode($return);
 echo $return['msg'];
 
