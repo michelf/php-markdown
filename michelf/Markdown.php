c8e00b8bab1f8c6e1e182b2f93058236884c36ba
Michel Fortin <michel.fortin@michelf.com>
2012-10-04 22:01:02 -0400

    Fixing issue #13 where no_markup / no_entities was ineffective on PHP Markdown Extra.

--- michelf/Markdown.php
+++ michelf/Markdown.php
@@ -1663,6 +1663,8 @@ class _MarkdownExtra_TmpImpl extends \michelf\Markdown {
 	#  _HashHTMLBlocks_InMarkdown to handle the Markdown syntax within the tag.
 	# These two functions are calling each other. It's recursive!
 	#
+		if ($this->no_markup)  return $text;
+
 		#
 		# Call the HTML-in-Markdown hasher.
 		#
