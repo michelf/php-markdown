f5cd0dbcf4d08cf7b0308ebf3e6eaba2dbab1776
Robbie Averill <robbie.averill@kathmandu.co.nz>
2016-02-25 09:27:26 +1300

    [#98] Changing to PHPDoc comments

--- Michelf/MarkdownInterface.inc.php
+++ Michelf/MarkdownInterface.inc.php
@@ -1,9 +1,15 @@
 <?php
-
-# Use this file if you cannot use class autoloading. It will include all the
-# files needed for the MarkdownInterface interface.
-#
-# Take a look at the PSR-0-compatible class autoloading implementation
-# in the Readme.php file if you want a simple autoloader setup.
+/**
+ * Use this file if you cannot use class autoloading. It will include all the
+ * files needed for the MarkdownInterface interface.
+ *
+ * Take a look at the PSR-0-compatible class autoloading implementation
+ * in the Readme.php file if you want a simple autoloader setup.
+ *
+ * @package   php-markdown
+ * @author    Michael Fortin <michael.fortin@michaelf.com>
+ * @copyright 2004-2016 Michel Fortin <http://michelf.com/projects/php-markdown/>
+ * @copyright (Original Markdown) 2004-2006 John Gruber <http://daringfireball.net/projects/markdown/>
+ */
 
 require_once dirname(__FILE__) . '/MarkdownInterface.php';
--- Readme.php
+++ Readme.php
@@ -1,31 +1,41 @@
 <?php
+/**
+ * This file passes the content of the Readme.md file in the same directory
+ * through the Markdown filter. You can adapt this sample code in any way
+ * you like.
+ *
+ * @package   php-markdown
+ * @author    Michael Fortin <michael.fortin@michaelf.com>
+ * @copyright 2004-2016 Michel Fortin <http://michelf.com/projects/php-markdown/>
+ * @copyright (Original Markdown) 2004-2006 John Gruber <http://daringfireball.net/projects/markdown/>
+ */
 
-# This file passes the content of the Readme.md file in the same directory
-# through the Markdown filter. You can adapt this sample code in any way
-# you like.
-
-# Install PSR-0-compatible class autoloader
-spl_autoload_register(function($class){
-	require preg_replace('{\\\\|_(?!.*\\\\)}', DIRECTORY_SEPARATOR, ltrim($class, '\\')).'.php';
+// Install PSR-0-compatible class autoloader
+spl_autoload_register(function($class) {
+	require preg_replace(
+		'{\\\\|_(?!.*\\\\)}',
+		DIRECTORY_SEPARATOR,
+		ltrim($class, '\\')
+	) . '.php';
 });
 
-# Get Markdown class
+// Get Markdown class
 use \Michelf\Markdown;
 
-# Read file and pass content through the Markdown parser
+// Read file and pass content through the Markdown parser
 $text = file_get_contents('Readme.md');
 $html = Markdown::defaultTransform($text);
 
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>PHP Markdown Lib - Readme</title>
-    </head>
-    <body>
+	<head>
+		<title>PHP Markdown Lib - Readme</title>
+	</head>
+	<body>
 		<?php
-			# Put HTML content in the document
-			echo $html;
+		// Put HTML content in the document
+		echo $html;
 		?>
-    </body>
+	</body>
 </html>
