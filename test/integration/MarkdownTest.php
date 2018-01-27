8376d3ecb87428112697a83a10712d4806815393
Michael Butler <michael@butlerpc.net>
2018-01-27 13:58:49 -0500

    Remove unused tests

--- test/integration/MarkdownTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use PHPUnit\Framework\TestCase;
-
-class MarkdownTest extends TestCase
-{
-
-}
--- test/integration/PhpMarkdownExtraTest.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use PHPUnit\Framework\TestCase;
-
-class PhpMarkdownExtraTest extends TestCase
-{
-
-}
