a1950a9273bb7e5283670faca8a89a0051ae5da7
Michael Butler <michael@butlerpc.net>
2018-03-17 14:49:07 -0400

    Run markdown.mdtest and php-markdown.mdtest against MarkdownExtra , except for Emphasis which is the exception

--- test/integration/PhpMarkdownTest.php
+++ test/integration/PhpMarkdownTest.php
@@ -7,15 +7,18 @@ use Michelf\MarkdownExtra;
 class PhpMarkdownTest extends TestCase
 {
 	/**
+	 * Returns all php-markdown.mdtest tests
 	 * @return array
 	 */
-	public function dataProviderForRegular() {
+	public function dataProviderForPhpMarkdown() {
 		$dir = TEST_RESOURCES_ROOT . '/php-markdown.mdtest';
 		return MarkdownTestHelper::getInputOutputPaths($dir);
 	}
 
 	/**
-	 * @dataProvider dataProviderForRegular
+	 * Runs php-markdown.mdtest against Markdown::defaultTransform
+	 *
+	 * @dataProvider dataProviderForPhpMarkdown
 	 *
 	 * @param string $inputPath Input markdown path
 	 * @param string $htmlPath File path of expected transformed output (X)HTML
@@ -28,6 +31,49 @@ class PhpMarkdownTest extends TestCase
 		$inputMarkdown = file_get_contents($inputPath);
 		$expectedHtml = file_get_contents($htmlPath);
 		$result = Markdown::defaultTransform($inputMarkdown);
+
+		MarkdownTestHelper::assertSameNormalized(
+			$expectedHtml,
+			$result,
+			"Markdown in $inputPath converts exactly to expected $htmlPath",
+			$xhtml
+		);
+	}
+
+	/**
+	 * Returns all php-markdown.mdtest tests EXCEPT Emphasis test.
+	 * @return array
+	 */
+	public function dataProviderForPhpMarkdownExceptEmphasis()
+	{
+		$dir = TEST_RESOURCES_ROOT . '/php-markdown.mdtest';
+		$allTests = MarkdownTestHelper::getInputOutputPaths($dir);
+
+		foreach ($allTests as $index => $test) {
+			// Because MarkdownExtra treats underscore emphasis differently, this one test has to be excluded
+			if (preg_match('~/Emphasis\.text$~', $test[0])) {
+				unset($allTests[$index]);
+			}
+		}
+
+		return array_values($allTests);
+	}
+
+	/**
+	 * Runs php-markdown.mdtest against MarkdownExtra::defaultTransform
+	 *
+	 * @dataProvider dataProviderForPhpMarkdownExceptEmphasis
+	 *
+	 * @param $inputPath
+	 * @param $htmlPath
+	 * @param bool $xhtml
+	 */
+	public function testPhpMarkdownMdTestWithMarkdownExtra($inputPath, $htmlPath, $xhtml = false)
+	{
+		$inputMarkdown = file_get_contents($inputPath);
+		$expectedHtml = file_get_contents($htmlPath);
+
+		$result = MarkdownExtra::defaultTransform($inputMarkdown);
 
 		MarkdownTestHelper::assertSameNormalized(
 			$expectedHtml,
@@ -92,6 +138,30 @@ class PhpMarkdownTest extends TestCase
 		$inputMarkdown = file_get_contents($inputPath);
 		$expectedHtml = file_get_contents($htmlPath);
 		$result = Markdown::defaultTransform($inputMarkdown);
+
+		MarkdownTestHelper::assertSameNormalized(
+			$expectedHtml,
+			$result,
+			"Markdown in $inputPath converts exactly to expected $htmlPath",
+			$xhtml
+		);
+	}
+
+	/**
+	 * Runs markdown.mdtest against MarkdownExtra::defaultTransform
+	 *
+	 * @dataProvider dataProviderForRegularMarkdown
+	 *
+	 * @param $inputPath
+	 * @param $htmlPath
+	 * @param bool $xhtml
+	 */
+	public function testMarkdownMdTestWithMarkdownExtra($inputPath, $htmlPath, $xhtml = false)
+	{
+		$inputMarkdown = file_get_contents($inputPath);
+		$expectedHtml = file_get_contents($htmlPath);
+
+		$result = MarkdownExtra::defaultTransform($inputMarkdown);
 
 		MarkdownTestHelper::assertSameNormalized(
 			$expectedHtml,
